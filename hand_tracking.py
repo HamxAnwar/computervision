e0d91a35e3c7270cd85e01a57dec52c3b87f381c
HamxAnwar <hamzaanwar93@outlook.com>
2023-10-07 00:23:15 +0500

    CMakeList and FPS display

--- hand_tracking.py
+++ hand_tracking.py
@@ -9,6 +9,10 @@ mpHands = mp.solutions.hands
 mpDraw = mp.solutions.drawing_utils
 hands = mpHands.Hands()
 
+#for FPS calculation
+previous_time = 0
+current_time = 0
+
 while True:
     success, image = cap.read()
     imageRGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
@@ -18,6 +22,11 @@ while True:
     if track.multi_hand_landmarks:
         for landmarks in track.multi_hand_landmarks:
             mpDraw.draw_landmarks(image, landmarks, mpHands.HAND_CONNECTIONS)
+
+    current_time = time.time()
+    fps = 1/(current_time - previous_time)
+    previous_time = current_time
+    cv2.putText(image, str(int(fps)), (5, 80), cv2.FONT_HERSHEY_PLAIN, 3, (0, 255, 0), 2)
 
     cv2.imshow("Hand tracked image", image)
     cv2.waitKey(1)
